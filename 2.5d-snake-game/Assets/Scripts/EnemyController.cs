2cd95a57d2429647d9f0dea65031c80326843a84
Joey L. Maalouf <joeylmaalouf@gmail.com>
2014-11-25 16:13:27 -0500

    did basically everything

---
+++ 2.5d-snake-game/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public float speed;
+	public GameObject player;
+
+	void FixedUpdate()
+	{
+		float horiz  = 1.0f * (this.player.transform.position.x > this.transform.position.x ? 1 : -1);
+		float vert = 1.0f * (this.player.transform.position.z > this.transform.position.z ? 1 : -1);
+		Vector3 movement = new Vector3
+		(
+			horiz,
+			0.0f,
+			vert
+		);
+		this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+	}
+}
---
+++ 2.5d-snake-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public GameObject enemy;
+	public GUIText scoreText;
+	public GUIText statusText;
+	private bool alive;
+	private int score;
+
+	void Start()
+	{
+		this.alive = true;
+		this.score = 0;
+		this.UpdateText();
+		this.statusText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		if (this.alive)
+		{
+			Vector3 movement = new Vector3
+			(
+				Input.GetAxis("Horizontal"),
+				0.0f,
+				Input.GetAxis("Vertical")
+			);
+			this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+		}
+		else
+		{
+			this.rigidbody.velocity = Vector3.zero;
+			this.rigidbody.angularVelocity = Vector3.zero;
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.transform.position = new Vector3
+			(
+				Random.Range(-11.1f, 11.1f),
+				0.5f,
+				Random.Range(-11.1f, 11.1f)
+			);
+			++this.score;
+			this.UpdateText();
+			this.audio.Play();
+			this.speed += 20;
+			if (this.score % 2 == 0)
+				Instantiate
+				(
+					enemy,
+					new Vector3
+					(
+						Random.Range(-11.1f, 11.1f),
+						0.5f,
+						Random.Range(-11.1f, 11.1f)
+					),
+					Quaternion.identity
+				);
+		}
+		else if (other.gameObject.tag == "Enemy")
+		{
+			this.alive = false;
+			this.statusText.text = "You Lose!\nBetter luck next time!";
+		}
+	}
+
+	void UpdateText()
+	{
+		this.scoreText.text = "Score: " + this.score.ToString();
+		if (this.score >= 100) this.statusText.text = "You Win!\nCongratulations!";
+	}
+}
